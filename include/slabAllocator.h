6873c1f10e8b645618749b15d4f039290d9d4a66
Marco Giordano <marco.giordano.work@gmail.com>
2017-09-21 18:39:36 +1000

    added slab alloc documentation

--- include/slabAllocator.h
+++ include/slabAllocator.h
@@ -6,30 +6,59 @@
 namespace babycpp {
 namespace memory {
 
+/**
+ * Simple struct representing an area of allocation
+ * @param data the start of the alloated memory
+ * @param rsp short for stack pointer, represent where
+ * the currently allocated user memory ends
+ * @param endp represent the end of the slab, aka system
+ * allocated memory
+ */
 struct Slab {
   char *data;
   char *rsp;
   char *endp;
 };
 
-// default slab size two MB for the use case should be more than enough
+/**
+ * Default size for slabs, if not provided this will be used
+ */
 const uint32_t SLAB_SIZE = 1 << 21;
 // Allocator interface
 struct SlabAllocator : Allocator {
+  /**
+   * Constructor
+   * @param slabSizeInByte how biges are the slabs in bites,
+   *        each new slab will have this size
+   */
   explicit SlabAllocator(int slabSizeInByte = SLAB_SIZE);
+  /**
+   * Allocates the requested amount of memory
+   * @param byteSize the size of the requested allocation in bytes
+   * @return pointer to the start of allocated memory
+   */
   void *alloc(uint32_t byteSize) override;
-  /*!
+  /**
    * @brief clear the allocation memory
    * Clears the internal memory and all the slabs, only
    * leaves a single slab allocated and empty
    * WARNING: the allocator does not call any kind of free
    * on the content of the memory, but frees only the whole
-   * h
    */
   void clear() override;
+  /**
+   * @brief Allocates a new slabs an returns a ref to it
+   * @return  newly allocated slab
+   */
   Slab &allocateSlab();
   virtual ~SlabAllocator() = default;
 
+  /**
+   * Automatically computes the offet between the base
+   * and the stack pointer, this offset represent the amount
+   * of allocation in bytes
+   * @return offets in byte
+   */
   inline uint64_t getStackPtrOffset() const {
     if (currentSlab != nullptr) {
       return (currentSlab->rsp - currentSlab->data);
@@ -38,7 +67,14 @@ struct SlabAllocator : Allocator {
   }
 
   // data
+  /**
+   * All the allocated slabs
+   */
   std::vector<Slab> slabs;
+  /**
+   * Currently in used slabs, when one is full, a new
+   * one gets allocated and this pointer changed to the new one
+   */
   Slab *currentSlab = nullptr;
   uint32_t slabSize;
 
