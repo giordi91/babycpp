1e99d70d72f93574222346bc82ce0ed63d2c9b58
Marco Giordano <marco.giordano.work@gmail.com>
2017-09-23 09:32:35 +1000

    added extra test for jitting functions

--- tests/jit/jitTests.cpp
+++ tests/jit/jitTests.cpp
@@ -13,6 +13,7 @@ inline std::string getFile(const ::std::string &path) {
   return str;
 }
 using babycpp::codegen::Codegenerator;
+
 TEST_CASE("Testing jit add", "[jit]") {
 
   babycpp::jit::BabycppJIT jit;
@@ -30,4 +31,43 @@ TEST_CASE("Testing jit add", "[jit]") {
   auto symbol = jit.findSymbol("test");
   auto func = (float (*)(float))(intptr_t)llvm::cantFail(symbol.getAddress());
   REQUIRE(func(10) == 11);
+}
+
+TEST_CASE("Testing jit complex  add", "[jit]") {
+
+  babycpp::jit::BabycppJIT jit;
+  Codegenerator gen;
+  gen.initFromString("float complexAdd(float x, int y){ return x+y;}");
+  // making sure parsing went fine
+  auto p = gen.parser.parseFunction();
+  REQUIRE(p != nullptr);
+
+  // making sure the code is generated
+  auto v = p->codegen(&gen);
+  REQUIRE(v != nullptr);
+
+  jit.addModule(gen.module);
+  auto symbol = jit.findSymbol("complexAdd");
+  auto func = (float (*)(float, int))(intptr_t)llvm::cantFail(symbol.getAddress());
+  REQUIRE(func(10.0f, 5) == Approx(15.0f));
+}
+
+TEST_CASE("Testing jit alloca", "[jit]") {
+
+  babycpp::jit::BabycppJIT jit;
+  Codegenerator gen;
+  gen.initFromString("float allocaTest(float x){ float temp = x * 2.0; temp = "
+                     "x - 2.0; return temp;}");
+  // making sure parsing went fine
+  auto p = gen.parser.parseFunction();
+  REQUIRE(p != nullptr);
+
+  // making sure the code is generated
+  auto v = p->codegen(&gen);
+  REQUIRE(v != nullptr);
+
+  jit.addModule(gen.module);
+  auto symbol = jit.findSymbol("allocaTest");
+  auto func = (float (*)(float ))(intptr_t)llvm::cantFail(symbol.getAddress());
+  REQUIRE(func(10.0f) == Approx(8.0f));
 }
